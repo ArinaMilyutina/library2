0190d7d81eff03b913147555ce8e6b117e9d970c
Arina <arina.milyutina.0005@gmail.com>
2024-12-01 21:44:00 +0300

    changed the books and book_genres tables, added column admin_id in books and added property unique for ISBN

--- src/main/resources/db.migration/V1_books.sql
+++ src/main/resources/db.migration/V1_books.sql
@@ -5,26 +5,13 @@ CREATE TABLE books
     author      VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     ISBN        VARCHAR(255) NOT NULL
-
 );
-CREATE TABLE genres
-(
-    id         SERIAL PRIMARY KEY,
-    genre_name VARCHAR(100) NOT NULL UNIQUE
-);
-
-CREATE TABLE book_genres
-(
-    book_id BIGINT       NOT NULL,
-    genre   VARCHAR(255) NOT NULL,
-    PRIMARY KEY (book_id, genre),
-    FOREIGN KEY (book_id) REFERENCES books (id) ON DELETE CASCADE
-);
-DROP TABLE book_genres;
 CREATE TABLE book_genres (
-                             book_id BIGINT,
-                             genre_id BIGINT,
-                             PRIMARY KEY (book_id, genre_id),
-                             FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE,
-                             FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
+                             book_id BIGINT REFERENCES books(id) ON DELETE CASCADE,
+                             genre VARCHAR(255) NOT NULL,
+                             PRIMARY KEY (book_id, genre)
 );
+ALTER TABLE books
+    ADD COLUMN admin_id BIGINT REFERENCES users(id) ON DELETE SET NULL;
+ALTER TABLE books
+    ADD CONSTRAINT unique_ISBN UNIQUE (ISBN);
