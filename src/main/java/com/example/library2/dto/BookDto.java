2af441df9753f2ba983013cf3200403bcaa7b9ab
Arina <arina.milyutina.0005@gmail.com>
2024-12-01 21:49:40 +0300

    added field admin,added data validation

---
+++ src/main/java/com/example/library2/dto/BookDto.java
@@ -0,0 +1,34 @@
+package com.example.library2.dto;
+
+import com.example.library2.entity.book.Genre;
+import com.example.library2.entity.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.*;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDto {
+    @NotBlank(message = "Title cannot be blank")
+    @Size(min = 1, max = 255, message = "Title must be between 1 and 255 characters")
+    private String title;
+    @NotBlank(message = "Title cannot be blank")
+    @Size(min =1, max = 255, message = "Title must be between 1 and 255 characters")
+    private String author;
+    @Pattern(regexp = "^\\d{13}$", message = "ISBN must consist of 13 digits!!!")
+    private String ISBN;
+    @NotBlank(message = "Title cannot be blank")
+    @Size(min = 1, max = 255, message = "Title must be between 1 and 255 characters")
+    private String description;
+    @NotNull(message = "Genre cannot be null")
+    @NotEmpty(message = "Genre cannot be empty")
+    @Enumerated(EnumType.STRING)
+    private Set<Genre> genre;
+    private User admin;
+}
