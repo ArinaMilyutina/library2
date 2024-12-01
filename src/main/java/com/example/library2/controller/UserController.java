7baabf178ff3e45d58c9c18d37b5b09ced8a29dd
Arina <arina.milyutina.0005@gmail.com>
2024-12-01 21:50:39 +0300

    created UserController with method for reg (admin and user) and auth user

---
+++ src/main/java/com/example/library2/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.library2.controller;
+
+import com.example.library2.dto.user.AuthUserDto;
+import com.example.library2.dto.user.RegUserDto;
+import com.example.library2.entity.user.Role;
+import com.example.library2.entity.user.User;
+import com.example.library2.service.AuthenticationService;
+import com.example.library2.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/reg")
+    public ResponseEntity<User> registrationUser(@RequestBody RegUserDto userDto) {
+        userDto.setRoles(Set.of(Role.USER));
+        return ResponseEntity.ok(userService.createUser(userDto));
+    }
+
+    @PostMapping("/reg/admin")
+    public ResponseEntity<User> registrationAdmin(@RequestBody RegUserDto userDto) {
+        userDto.setRoles(Set.of(Role.ADMIN));
+        return ResponseEntity.ok(userService.createUser(userDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody AuthUserDto authUserDto) {
+        String token = authenticationService.authenticate(authUserDto);
+        return ResponseEntity.ok(token);
+    }
+}
