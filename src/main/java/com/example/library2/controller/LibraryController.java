0f6c16b9b02be2eb5442715f0a1e47c5b2aca511
Arina <arina.milyutina.0005@gmail.com>
2024-12-03 02:01:04 +0300

    created controller with post( create book) and get (get available books) method

---
+++ src/main/java/com/example/library2/controller/LibraryController.java
@@ -0,0 +1,29 @@
+package com.example.library2.controller;
+
+import com.example.library2.dto.LibraryDto;
+import com.example.library2.entity.Library;
+import com.example.library2.entity.book.Book;
+import com.example.library2.exception.NotFoundException;
+import com.example.library2.service.LibraryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/library")
+public class LibraryController {
+    @Autowired
+    private LibraryService libraryService;
+    @PostMapping("/borrow")
+    public ResponseEntity<Library> createBook(@RequestParam String username, @RequestParam String isbn, @RequestBody LibraryDto libraryDto) {
+        Library libraryEntry = libraryService.createLibrary(username, isbn, libraryDto);
+        return ResponseEntity.ok(libraryEntry);
+    }
+    @GetMapping("/available-books")
+    public ResponseEntity<List<Book>> getAvailableBooks() throws NotFoundException {
+        List<Book> availableBooks = libraryService.getAvailableBooks();
+        return ResponseEntity.ok(availableBooks);
+    }
+}
