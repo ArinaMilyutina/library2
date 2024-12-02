853023e1178a140179f6247002d989a317724855
Arina <arina.milyutina.0005@gmail.com>
2024-12-03 02:01:25 +0300

    dto for library

---
+++ src/main/java/com/example/library2/dto/LibraryDto.java
@@ -0,0 +1,14 @@
+package com.example.library2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LibraryDto {
+    private LocalDateTime returnDate;
+}
