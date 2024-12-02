fc7fb44f5017fc37414c28f6020b3a8d7b842c27
Arina <arina.milyutina.0005@gmail.com>
2024-12-02 13:30:29 +0300

    implemented method for crud operation(get all books,find book by id, find by isbn, delete book by isbn, update book by isbn

--- src/main/java/com/example/library2/controller/BookController.java
+++ src/main/java/com/example/library2/controller/BookController.java
@@ -1,19 +1,19 @@
 package com.example.library2.controller;
 
 import com.example.library2.dto.BookDto;
-import com.example.library2.entity.book.Book;
+import com.example.library2.entity.book.Book;import com.example.library2.exception.NotFoundException;
 import com.example.library2.service.BookService;
 import com.example.library2.service.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 
 @RestController
@@ -29,5 +29,40 @@ public class BookController {
     public ResponseEntity<Book> createBook(@Valid @RequestBody BookDto bookDto) {
         bookDto.setAdmin(securityService.getCurrentUser());
         return ResponseEntity.status(HttpStatus.CREATED).body(bookService.createBook(bookDto));
+    }
+
+    @GetMapping("/books")
+    public ResponseEntity<List<Book>> findAll() throws NotFoundException {
+        return ResponseEntity.of(Optional.of(bookService.findAll()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> findById(@PathVariable Long id) throws NotFoundException {
+        Optional<Book> book = bookService.findById(id);
+        return book.map(ResponseEntity::ok)
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/ISBN/{ISBN}")
+    public ResponseEntity<Book> findByISBN(@PathVariable String ISBN) throws NotFoundException {
+        Optional<Book> book = bookService.findByISBN(ISBN);
+        return book.map(ResponseEntity::ok)
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/delete/{ISBN}")
+    public ResponseEntity<String> deleteByISBN(@PathVariable String ISBN) {
+        return new ResponseEntity<>(bookService.deleteBookByISBN(ISBN), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/update/{ISBN}")
+    public ResponseEntity<Book> updateByISBN(@PathVariable String ISBN, @Valid @RequestBody BookDto bookDto) {
+        bookDto.setAdmin(securityService.getCurrentUser());
+        Book book = bookService.updateBookByISBN(ISBN, bookDto);
+        return ResponseEntity.ok(book);
+
     }
 }
--- src/main/java/com/example/library2/service/BookService.java
+++ src/main/java/com/example/library2/service/BookService.java
@@ -2,17 +2,23 @@ package com.example.library2.service;
 
 import com.example.library2.dto.BookDto;
 import com.example.library2.entity.book.Book;
+import com.example.library2.exception.NotFoundException;
 import com.example.library2.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
     @Autowired
     private BookRepository bookRepository;
+
 
     public Book createBook(BookDto bookDto) {
         try {
@@ -28,5 +34,40 @@ public class BookService {
         } catch (DataIntegrityViolationException e) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "ISBN already exists");
         }
+    }
+
+    public List<Book> findAll() throws NotFoundException {
+        List<Book> bookList = bookRepository.findAll();
+        if (bookList.isEmpty()) {
+            throw new NotFoundException("Books not found");
+        }
+        return bookList;
+    }
+
+    public Optional<Book> findById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Optional<Book> findByISBN(String ISBN) {
+        return bookRepository.findByISBN(ISBN);
+    }
+
+    @Transactional
+    public String deleteBookByISBN(String ISBN) {
+        bookRepository.deleteByISBN(ISBN);
+        return "The book has been deleted";
+    }
+
+    public Book updateBookByISBN(String ISBN, BookDto bookDto) {
+        Optional<Book> byISBN = findByISBN(ISBN);
+        Book book = Book.builder()
+                .title(bookDto.getTitle())
+                .author(bookDto.getAuthor())
+                .description(bookDto.getDescription())
+                .ISBN(bookDto.getDescription())
+                .genre(bookDto.getGenre())
+                .admin(bookDto.getAdmin())
+                .build();
+        return bookRepository.save(book);
     }
 }
