78fec86200c9ee8d422aac3608c828396553d771
Arina <arina.milyutina.0005@gmail.com>
2024-12-03 02:03:00 +0300

    created service for user and realized methods ( create user and find user by username)

---
+++ src/main/java/com/example/library2/service/UserService.java
@@ -0,0 +1,55 @@
+package com.example.library2.service;
+
+import com.example.library2.dto.user.RegUserDto;
+import com.example.library2.entity.user.Role;
+import com.example.library2.entity.user.User;
+import com.example.library2.exception.UsernameAlreadyExistsException;
+import com.example.library2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class UserService implements UserDetailsService {
+    private static final String USER_NOT_FOUND = "User not found: ";
+    private static final String USER_ALREADY_EXISTS = "Username already exists";
+
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(RegUserDto userDto) {
+        try {
+            User user = User.builder().username(userDto.getUsername()).password(passwordEncoder().encode(userDto.getPassword())).name(userDto.getName()).roles(Set.of(userDto.getRoles().toArray(new Role[0]))).build();
+            return userRepository.save(user);
+        } catch (DataIntegrityViolationException e) {
+            throw new UsernameAlreadyExistsException(USER_ALREADY_EXISTS);
+        }
+
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return Optional.ofNullable(userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND + username)));
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> byUsername = userRepository.findUserByUsername(username);
+        if (byUsername.isPresent()) {
+            return byUsername.get();
+        }
+        throw new UsernameNotFoundException(USER_NOT_FOUND);
+    }
+}
