8c4307eaa4b455ac78989ad995e9ae8244dde69e
Arina <arina.milyutina.0005@gmail.com>
2024-12-02 13:31:07 +0300

    added method for find book by isbn and delete by isbn

--- src/main/java/com/example/library2/repository/BookRepository.java
+++ src/main/java/com/example/library2/repository/BookRepository.java
@@ -4,6 +4,12 @@ import com.example.library2.entity.book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByISBN(String ISBN);
+
+    void deleteByISBN(String ISBN);
+
 }
