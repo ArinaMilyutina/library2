b1cd960c312f7de6073336abd61deffc3afa232e
Arina <arina.milyutina.0005@gmail.com>
2024-12-04 02:08:51 +0300

    added validation data

--- src/main/java/com/example/library2/dto/user/RegUserDto.java
+++ src/main/java/com/example/library2/dto/user/RegUserDto.java
@@ -7,14 +7,22 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegUserDto {
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 4, max = 100, message = "Username must be between 4 and 100 characters")
     private String username;
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{6,}$", message = "Password must consist of at least 6 characters(mandatory to use lowercase,uppercase and 0-9 digital)!!!")
     private String password;
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 1, max = 100, message = "Name must be between 1 and 100 characters")
     private String name;
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
