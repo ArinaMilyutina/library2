ea7ac06661fdb29132a00908a83e5ebee1ad0d49
Arina <arina.milyutina.0005@gmail.com>
2024-12-04 02:08:32 +0300

    corrected msg in validation

--- src/main/java/com/example/library2/dto/BookDto.java
+++ src/main/java/com/example/library2/dto/BookDto.java
@@ -18,13 +18,13 @@ public class BookDto {
     @NotBlank(message = "Title cannot be blank")
     @Size(min = 1, max = 255, message = "Title must be between 1 and 255 characters")
     private String title;
-    @NotBlank(message = "Title cannot be blank")
-    @Size(min =1, max = 255, message = "Title must be between 1 and 255 characters")
+    @NotBlank(message = "Author cannot be blank")
+    @Size(min =1, max = 255, message = "Author must be between 1 and 255 characters")
     private String author;
     @Pattern(regexp = "^\\d{13}$", message = "ISBN must consist of 13 digits!!!")
     private String ISBN;
-    @NotBlank(message = "Title cannot be blank")
-    @Size(min = 1, max = 255, message = "Title must be between 1 and 255 characters")
+    @NotBlank(message = "Description cannot be blank")
+    @Size(min = 1, max = 255, message = "Description must be between 1 and 255 characters")
     private String description;
     @NotNull(message = "Genre cannot be null")
     @NotEmpty(message = "Genre cannot be empty")
