601ffdb532bf56ce34667075adb5090eab23b7f7
Arina <arina.milyutina.0005@gmail.com>
2024-12-03 16:37:37 +0300

    added url for role admin

--- src/main/java/com/example/library2/controller/LibraryController.java
+++ src/main/java/com/example/library2/controller/LibraryController.java
@@ -7,6 +7,7 @@ import com.example.library2.exception.NotFoundException;
 import com.example.library2.service.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,11 +17,14 @@ import java.util.List;
 public class LibraryController {
     @Autowired
     private LibraryService libraryService;
-    @PostMapping("/borrow")
+
+    @PreAuthorize(value = "hasRole('ADMIN')")
+    @PostMapping("/admin/create")
     public ResponseEntity<Library> createBook(@RequestParam String username, @RequestParam String isbn, @RequestBody LibraryDto libraryDto) {
         Library libraryEntry = libraryService.createLibrary(username, isbn, libraryDto);
         return ResponseEntity.ok(libraryEntry);
     }
+
     @GetMapping("/available-books")
     public ResponseEntity<List<Book>> getAvailableBooks() throws NotFoundException {
         List<Book> availableBooks = libraryService.getAvailableBooks();
