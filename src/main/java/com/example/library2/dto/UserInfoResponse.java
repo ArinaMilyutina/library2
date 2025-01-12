501f1a9cdac830886ece77ff5f35ce888ea5797b
Arina <arina.milyutina.0005@gmail.com>
2025-01-12 18:24:31 +0300

    created UserInfoResponse to communication with book-service

---
+++ src/main/java/com/example/library2/dto/UserInfoResponse.java
@@ -0,0 +1,15 @@
+package com.example.library2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserInfoResponse {
+    private Long userId;
+    private String username;
+}
