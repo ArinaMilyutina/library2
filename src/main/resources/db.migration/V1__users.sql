aaea96e116f2c0ac7d362fca4806fa22e2e15bb5
Arina <arina.milyutina.0005@gmail.com>
2025-01-12 17:55:25 +0300

    added sql for created library2_schema

--- src/main/resources/db.migration/V1__users.sql
+++ src/main/resources/db.migration/V1__users.sql
@@ -1,12 +1,23 @@
-CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       username VARCHAR(100) NOT NULL UNIQUE,
-                       password VARCHAR(100) NOT NULL,
-                       name VARCHAR(100) NOT NULL
+DO
+$$
+    BEGIN
+        IF
+            NOT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = 'library2_schema') THEN
+            CREATE SCHEMA library2_schema;
+        END IF;
+    END
+$$;
+CREATE TABLE library2_schema.users (
+                                       id SERIAL PRIMARY KEY,
+                                       username VARCHAR(100) NOT NULL UNIQUE,
+                                       password VARCHAR(100) NOT NULL,
+                                       name VARCHAR(100) NOT NULL
 );
-CREATE TABLE user_roles (
-                            user_id BIGINT NOT NULL,
-                            role VARCHAR(50) NOT NULL,
-                            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-                            PRIMARY KEY (user_id, role)
+
+CREATE TABLE library2_schema.user_roles (
+                                            user_id BIGINT NOT NULL,
+                                            role VARCHAR(50) NOT NULL,
+                                            FOREIGN KEY (user_id) REFERENCES library2_schema.users(id) ON DELETE CASCADE,
+                                            PRIMARY KEY (user_id, role)
 );
+
