b832a701829cc4d92c01095e29c535ebcce0f405
Arina <arina.milyutina.0005@gmail.com>
2025-01-12 18:22:21 +0300

    deleted package user(entity.user.User - > entity.User)

--- src/main/java/com/example/library2/repository/UserRepository.java
+++ src/main/java/com/example/library2/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.library2.repository;
 
-import com.example.library2.entity.user.User;
+import com.example.library2.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
