bdaa76b1f4c6808016e1241f053a128c9577d959
Arina <arina.milyutina.0005@gmail.com>
2025-01-12 17:57:50 +0300

    deleted UserController and divided external and internal controller

---
+++ src/main/java/com/example/library2/controller/external/ExternalUserController.java
@@ -0,0 +1,36 @@
+package com.example.library2.controller.external;
+
+import com.example.library2.dto.UserInfoRequest;
+import com.example.library2.entity.User;
+import com.example.library2.exception.NotFoundException;
+import com.example.library2.service.SecurityService;
+import com.example.library2.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/external/user")
+public class ExternalUserController {
+
+    @Autowired
+    private SecurityService securityService;
+    @Autowired
+    private UserService userService;
+
+    @PreAuthorize(value = "hasRole('ADMIN')")
+    @GetMapping("/current-user")
+    public ResponseEntity<Long> getCurrentUser() {
+        User currentUser = securityService.getCurrentUser();
+        return ResponseEntity.ok(currentUser.getId());
+    }
+
+    @PreAuthorize(value = "hasRole('ADMIN')")
+    @GetMapping("/find-by-id/{id}")
+    public ResponseEntity<UserInfoRequest> findById(@PathVariable Long id) throws NotFoundException {
+        return ResponseEntity.ok(userService.takeTheBook(id));
+    }
+
+}
--- src/main/java/com/example/library2/controller/internal/InternalUserController.java
+++ src/main/java/com/example/library2/controller/internal/InternalUserController.java
@@ -1,39 +1,35 @@
-package com.example.library2.controller;
+package com.example.library2.controller.internal;
 
-import com.example.library2.dto.user.AuthUserDto;
-import com.example.library2.dto.user.RegUserDto;
-import com.example.library2.entity.user.Role;
-import com.example.library2.entity.user.User;
-import com.example.library2.exception.IncorrectPasswordException;
+import com.example.library2.dto.AuthUserDto;
+import com.example.library2.dto.RegUserDto;
+import com.example.library2.entity.Role;
+import com.example.library2.entity.User;
 import com.example.library2.exception.NotFoundException;
 import com.example.library2.service.AuthenticationService;
 import com.example.library2.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Set;
 
 @RestController
 @RequestMapping("/user")
-public class UserController {
+public class InternalUserController {
     @Autowired
     private AuthenticationService authenticationService;
     @Autowired
     private RegistrationService registrationService;
 
     @PostMapping("/reg")
-    public ResponseEntity<User> registrationUser(@Valid @RequestBody RegUserDto userDto){
+    public ResponseEntity<User> registrationUser(@Valid @RequestBody RegUserDto userDto) {
         userDto.setRoles(Set.of(Role.USER));
         return ResponseEntity.ok(registrationService.regUser(userDto));
     }
 
     @PostMapping("/reg/admin")
-    public ResponseEntity<User> registrationAdmin(@Valid @RequestBody RegUserDto userDto){
+    public ResponseEntity<User> registrationAdmin(@Valid @RequestBody RegUserDto userDto) {
         userDto.setRoles(Set.of(Role.ADMIN));
         return ResponseEntity.ok(registrationService.regUser(userDto));
     }
